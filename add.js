ba8a1c0cc7598d7df79552e17579932c1a56b6e0
Anchal Gupta <anchalbatham1729@gmail.com>
2024-02-25 12:06:13 +0530

    add.js
    calculator add function

---
+++ add.js
@@ -0,0 +1,11 @@
+//Add function
+
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Example usage:
+var number1 = 5;
+var number2 = 10;
+var sumResult = addNumbers(number1, number2);
+console.log(`The sum of ${number1} and ${number2} is: ${sumResult}`);
