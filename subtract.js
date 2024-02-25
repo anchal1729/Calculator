42356acf0c7328d956f7339aa55738c19a1f0f93
SauravSaurav04 <sauravsaurav4444@gmail.com>
2024-02-25 12:28:48 +0530

    'subtraction'

---
+++ subtract.js
@@ -0,0 +1,11 @@
+//subtraction function
+
+function subtractNumbers(num1, num2) {
+    return num1 - num2;
+}
+
+// Example usage:
+var number1 = 15;
+var number2 = 5;
+var subtractResult = subtractNumbers(number1, number2);
+console.log(`The subtraction of ${number1} and ${number2} is: ${subtractResult}`);
